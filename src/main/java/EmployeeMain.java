4cc1bb06579f3f6af5ba78aaa19d4472b832bbd0
manaalrehman <rehmm003@gmail.com>
2022-02-15 14:09:40 +0000

    Implementing Methods in EmployeeService.java Implementing Methods in Employee.java Implementing Methods in Address.java

--- src/main/java/EmployeeMain.java
+++ src/main/java/EmployeeMain.java
@@ -1,10 +1,11 @@
 import com.eainde.assignment.EmployeeService;
 
+import java.io.IOException;
 import java.util.WeakHashMap;
 
 public class EmployeeMain {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
         EmployeeService es1 = new EmployeeService();
         System.out.println(es1.getAverageSalary());
     }
